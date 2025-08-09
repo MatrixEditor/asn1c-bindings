8d7a2397a8ead977bc82f47d64fdff62edb32a06
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2025-08-09 12:43:04 +0200

    Split out pyhton code generation

---
+++ libasn1compiler/asn1c_Py.h
@@ -0,0 +1,15 @@
+#ifndef ASN1_COMPILER_LANDUAGE_PY_H
+#define ASN1_COMPILER_LANDUAGE_PY_H
+
+#include "asn1c_internal.h"
+#include "asn1c_lang.h"
+
+int asn1c_lang_Py_type_SEQUENCE(arg_t *);
+int asn1c_lang_Py_type_CHOICE(arg_t *);
+int asn1c_lang_Py_type_SIMPLE_TYPE(arg_t *);
+
+// int asn1c_lang_PyStub_type_SEQUENCE(arg_t *);
+// int asn1c_lang_PyStub_type_CHOICE(arg_t *);
+// int asn1c_lang_PyStub_type_SIMPLE_TYPE(arg_t *);
+
+#endif /* ASN1_COMPILER_LANDUAGE_PY_H */
