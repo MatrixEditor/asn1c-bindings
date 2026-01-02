6f837ea903dc86060c8ef70ffb5480f1c60acd1e
Mouse <5923577+mouse07410@users.noreply.github.com>
2026-01-02 14:30:47 -0500

    Remove unnecessary initialization of er variable
    Unused variable - removed

--- skeletons/OPEN_TYPE_xer.c
+++ skeletons/OPEN_TYPE_xer.c
@@ -282,8 +282,7 @@ asn_enc_rval_t
 OPEN_TYPE_encode_xer(const asn_TYPE_descriptor_t *td, const void *sptr,
                      int ilevel, enum xer_encoder_flags_e flags,
                      asn_app_consume_bytes_f *cb, void *app_key) {
-    asn_enc_rval_t er = {0,0,0};
-
+    
     if(!sptr)
         ASN__ENCODE_FAILED;
 
